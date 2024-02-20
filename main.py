28b3c905cec10f0d7740dca2278a8878d0cc973e
filin121666 <mur.my.75@mail.ru>
2024-02-20 19:23:59 +0300

    дз

---
+++ main.py
@@ -0,0 +1,19 @@
+import abc
+import random
+
+
+class change_damage:
+    def change_damage_for_warrior(weapon) -> int:
+        if 
+
+
+class Player(abc.ABC):
+    def attack():
+        pass
+
+
+class Warrior(Player):
+    def __init__(self, weapon: str) -> None:
+        super().__init__()
+        self.weapon = weapon.lower()
+    
