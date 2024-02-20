566922b659868868d32e129b8972442d717d2b81
filin121666 <mur.my.75@mail.ru>
2024-02-20 19:36:29 +0300

    дз

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ import random
 
 class change_damage:
     def change_damage_for_warrior(weapon) -> int:
-        if 
+        crit = random.choice([0, 0, 0, 0, 0, 0, 0, 0, 1, 1])
+        if crit:
+            if weapon == 'меч':
+                return random.randint(75, 100)
+            elif weapon == 'копьё':
+                return random.randint(90, 120)
+        else:
+            if weapon == 'меч':
+                return random.randint(60, 85)
+            elif weapon == 'копьё':
+                return random.randint(80, 100)
 
 
 class Player(abc.ABC):
@@ -13,6 +23,7 @@ class Player(abc.ABC):
 
 
 class Warrior(Player):
+    health = 1250
     def __init__(self, weapon: str) -> None:
         super().__init__()
         self.weapon = weapon.lower()
